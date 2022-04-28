1d5ab9468afdcba4f93f5c42b68d1ec7176565bf
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-04-28 10:20:00 +0200

    Mutex

--- synchronization/main.cpp
+++ synchronization/main.cpp
@@ -1,24 +1,57 @@
+#include "joining_thread.hpp"
+
 #include <cassert>
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <vector>
-#include "joining_thread.hpp"
 
 using namespace std::literals;
 
-void run(int& counter)
+template <typename T>
+struct Synchronized
 {
-    for(int i = 0; i < 1'000'000; ++i)
-        ++counter;
+    T value;
+    std::mutex mtx_value;
+};
+
+template <typename F, typename T>
+void apply(F f, Synchronized<T>& sync_value)
+{
+    std::lock_guard lk {sync_value.mtx_value};
+    f(sync_value.value);
+}
+
+void run(Synchronized<uint64_t>& counter)
+{
+    for (uint64_t i = 0; i < 1'000'000ULL; ++i)
+    {
+        // std::lock_guard lk{mtx_counter}; // SC begins
+        // ++counter;
+        
+        apply([&](uint64_t& i)
+            { ++i; },
+            counter);
+    } // SC ends
 }
 
 int main()
 {
     std::cout << "Main thread starts..." << std::endl;
     const std::string text = "Hello Threads";
+
+    Synchronized<uint64_t> counter{};
+
+    {
+        ext::joining_thread thd1 {run, std::ref(counter)};
+        ext::joining_thread thd2 {[&counter]
+            { run(counter); }};
+    } // implicit join
+
+    std::cout << "Counter: " << counter.value << std::endl;
 
     std::cout << "Main thread ends..." << std::endl;
 }
